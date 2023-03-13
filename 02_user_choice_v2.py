fbd75b0a75aaf833ae38220e7fb14d0149511daf
kabondok1177 <125608943+kabondok1177@users.noreply.github.com>
2023-03-14 08:30:40 +1300

    Add files via upload

--- 02_user_choice_v2.py
+++ 02_user_choice_v2.py
@@ -1,7 +1,7 @@
 # Version 2 - error message included when calling function
 
 
-# Funtions goes here
+# Functions goes here
 def choice_checker(question, error):
     valid = False
     while not valid:
@@ -21,3 +21,21 @@ def choice_checker(question, error):
             return 'xxx'
         else:
             print(error)
+
+# Main routine goes here
+
+
+# Loop for testing purposes
+user_choice = ""
+while user_choice != "xxx":
+
+    # Ask user for choice amd check it's valid
+    user_choice = choice_checker ("Choose rock / paper /"
+                                  "scissors (r/p/s):",
+                                  "Please choose from rock/"
+                                  "paper / scissors"
+                                  "(or xxx to quit)")
+
+    # print out choice for comparison purpose
+    print("You chose {}".format(user_choice))
+
