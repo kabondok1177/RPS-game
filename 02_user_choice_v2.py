788236059cb2cb4f06ffc05685297567f4c74303
kabondok1177 <125608943+kabondok1177@users.noreply.github.com>
2023-03-14 13:29:29 +1300

    Add files via upload

--- 02_user_choice_v2.py
+++ 02_user_choice_v2.py
@@ -22,20 +22,19 @@ def choice_checker(question, error):
         else:
             print(error)
 
+
 # Main routine goes here
 
 
 # Loop for testing purposes
 user_choice = ""
 while user_choice != "xxx":
-
     # Ask user for choice amd check it's valid
-    user_choice = choice_checker ("Choose rock / paper /"
-                                  "scissors (r/p/s):",
-                                  "Please choose from rock/"
-                                  "paper / scissors"
-                                  "(or xxx to quit)")
+    user_choice = choice_checker("Choose rock / paper /"
+                                 "scissors (r/p/s):",
+                                 "Please choose from rock/"
+                                 "paper / scissors"
+                                 "(or xxx to quit)")
 
     # print out choice for comparison purpose
     print("You chose {}".format(user_choice))
-
